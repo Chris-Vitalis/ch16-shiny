f88adf1953df15d5d9e7f7f232451315d35d56ee
Chris-Vitalis <cvitalis@uw.edu>
2018-05-09 22:22:15 -0700

    Finish Assignment

--- exercise-5/ui.R
+++ exercise-5/ui.R
@@ -30,7 +30,33 @@ shinyUI(navbarPage(
         plotlyOutput("map")
       )
     )
+  ),
+  tabPanel(
+    "Scatter",
+    titlePanel("Population v.s. Vote Power"),
+    # Create sidebar layout
+    sidebarLayout(
+      
+      # Side panel for controls
+      sidebarPanel(
+        
+        # Input to select variable to map
+        textInput(
+          "state",
+          label = "Find a State",
+          value = ""
+
+          )
+        )
+      ,
+      
+      # Main panel: display plotly map
+      mainPanel(
+        plotlyOutput("scattermap")
+      )
+    )
   )
 
-  # Create a tabPanel to show your scatter plot
+ 
 ))
+  # Create a tabPanel to show your scatter plot
